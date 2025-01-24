666004465dd42bf52467915d2fe3695a393315e8
Luis Lomeli <luis.lomeli@plasticosplasa.com>
2025-01-24 12:08:04 -0600

    fixing initrepo script

--- initrepo.sh
+++ initrepo.sh
@@ -1,8 +1,9 @@
+# Initialize and update submodules if the repo is already cloned
 git submodule init
-git submodule update
+# git submodule update
 
-git clone --recurse-submodules --depth 10 https://github.com/luisrlppg/PPG-ERP
-
+# Clone the repository with submodules if not already cloned
+git clone --recurse-submodules --depth 1 git@github.com:luisrlppg/PPG-ERP.git
 
 # to pull new changes from a sub module
 # git submodule update --init --depth 10
