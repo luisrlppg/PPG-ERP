49849f51ad408562b1b3c1febdb8a502bbc61fab
Luis Lomeli <luis.lomeli@plasticosplasa.com>
2025-01-24 11:42:22 -0600

    submodules initialization

--- docker/dev/17/getodoo.sh
+++
@@ -1,2 +0,0 @@
-git fetch --depth 1 origin 17.0
-git checkout 17.0
---
+++ initrepo.sh
@@ -0,0 +1,9 @@
+git submodule init
+git submodule update
+
+git clone --recurse-submodules --depth 10 https://github.com/luisrlppg/PPG-ERP
+
+
+# to pull new changes from a sub module
+# git submodule update --init --depth 10
+
