00d5a53050ff1a6edecc91c54265caa713f9cd37
Luis Lomeli <luis.lomeli@plasticosplasa.com>
2025-01-24 13:35:41 -0600

    updating modules

--- initrepo.sh
+++ initrepo.sh
@@ -1,9 +1,12 @@
+git submodule update --init --recursive --depth 1
+
+#git submodule add https://github.com/luisrlppg/odoo.git docker/dev/17/odoo
 # Initialize and update submodules if the repo is already cloned
-git submodule init
+#git submodule init
 # git submodule update
 
 # Clone the repository with submodules if not already cloned
-git clone --recurse-submodules --depth 1 git@github.com:luisrlppg/PPG-ERP.git
+#git clone --recurse-submodules --depth 1 git@github.com:luisrlppg/PPG-ERP.git
 
 # to pull new changes from a sub module
 # git submodule update --init --depth 10
